163898acfde78a61e0e213d619c0d72b06d6c699
mgasin <mgasin@lexmat.com>
2024-04-17 13:44:58 -0400

    feat: header component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,67 +1,126 @@
 import { cva } from 'class-variance-authority';
-import type { FC } from 'react';
+import { FC, ReactNode, useEffect, useState } from 'react';
 import { useTheme } from 'contexts/DarkModeContext';
 import { twMerge } from 'tailwind-merge';
-import type { LinkProps } from 'components/link';
-import Link from 'components/link';
 
 interface HeaderProps {
-  links: LinkProps[];
+  children?: ReactNode;
 }
 
-const header = cva(['sticky', 'top-0', 'w-full', 'px-8', 'py-4', 'shadow-sm', 'flex', 'justify-center'], {
-  variants: {
-    theme: {
-      light: ['bg-gray-200'],
-      dark: ['bg-gray-600'],
+const header = cva(
+  [
+    'fixed',
+    'top-0',
+    'w-full',
+    'px-8',
+    'py-4',
+    'shadow-sm',
+    'flex',
+    'z-50',
+    'justify-center',
+    'transition-opacity',
+    'duration-300',
+    'ease-in-out',
+  ],
+  {
+    variants: {
+      theme: {
+        light: ['bg-gray-200'],
+        dark: ['bg-gray-600'],
+      },
+      hidden: {
+        true: ['opacity-0'],
+        false: ['opacity-100'],
+      },
     },
   },
-});
+);
 
-const headerWrapper = cva([
-  'container',
-  'w-full',
-  'max-w-7xl',
-  'flex',
-  'flex-col',
-  'sm:flex-row',
-  'justify-between',
-  'items-center',
-]);
+const headerRow = cva(['flex', 'flex-row', 'gap-4', 'md:gap-12', 'flex-wrap']);
 
-const headerLink = cva(['flex', 'gap-4', 'items-center']);
-const headerLinkContent = cva(['transition-all', 'duration-300', 'ease-in-out'], {
-  variants: {
-    type: {
-      title: ['text-3xl'],
-      standard: ['text-md'],
-    },
-    theme: {
-      light: ['text-gray-700', 'hover:text-yellow-800'],
-      dark: ['text-gray-100', 'hover:text-yellow-100'],
+const headerEntry = cva(
+  [
+    'font-normal',
+    'text-xl',
+    'capitalize',
+    'relative',
+    'after:content-[ ]',
+    'after:origin-left',
+    'after:h-1',
+    'after:rounded-xl',
+    'after:w-full',
+    'after:absolute',
+    'after:left-0',
+    'after:-bottom-1',
+    'after:transition-transform',
+    'after:duration-300',
+    'after:ease-in-out',
+  ],
+  {
+    variants: {
+      theme: {
+        light: ['text-gray-700', 'hover:gray-800', 'after:bg-gray-800'],
+        dark: ['text-gray-100', 'hover:text-white', 'after:bg-white'],
+      },
+      isActive: {
+        true: ['after:scale-x-100'],
+        false: ['after:scale-x-0'],
+      },
+      hidden: {
+        true: ['hidden'],
+        false: ['visible'],
+      },
     },
   },
-});
-const linkWrapper = cva(['flex', 'gap-8']);
+);
 
-const Header: FC<HeaderProps> = ({ links, ...props }) => {
+const Header: FC<HeaderProps> = ({ ...props }) => {
   const { theme } = useTheme();
 
+  const [navItems, setNavItems] = useState<string[]>([]);
+  const [active, setActive] = useState<string>();
+  const [hidden, setHidden] = useState(true);
+
+  const hiddenThreshold = 64;
+  const handleScroll = () => {
+    setNavItems([...document.querySelectorAll('section')].map(node => node.id));
+    setActive(document.querySelector('.active')?.getAttribute('id') || '');
+    setHidden(window.scrollY < hiddenThreshold);
+  };
+
+  useEffect(() => {
+    handleScroll();
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const focusSection = (sectionId: string) => {
+    document.querySelector(`#${sectionId}`)?.scrollIntoView();
+  };
+
   return (
-    <header className={twMerge(header({ theme }))} {...props}>
-      <div className={twMerge(headerWrapper())}>
-        <Link className={twMerge(headerLink())} href="/" isInternalLink={true}>
-          <p className={twMerge(headerLinkContent({ theme, type: 'title' }))}>Shot Calculator</p>
-        </Link>
-        <div className={twMerge(linkWrapper())}>
-          {links &&
-            links.map(link => (
-              <Link {...link} key={link.id} className={twMerge(headerLinkContent({ theme, type: 'standard' }))}>
-                {link.children}
-              </Link>
-            ))}
-        </div>
-      </div>
+    <header className={twMerge(header({ theme, hidden }))} aria-hidden={hidden} {...props}>
+      <nav>
+        <ul className={twMerge(headerRow())}>
+          {navItems.map(entry => (
+            <li key={entry} className={twMerge(headerEntry({ theme, isActive: active === entry, hidden }))}>
+              <a
+                href="#"
+                onClick={e => {
+                  e.preventDefault();
+                  focusSection(entry);
+                  handleScroll();
+                }}
+              >
+                {entry}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 };
